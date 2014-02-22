cea5d1b988a8ee1afa29b0399e77fb67310ad98e
CaptainBern <captainberneby@gmail.com>
2014-02-22 22:55:32 +0100

    Fixed mess.

--- src/main/java/com/dsh105/holoapi/util/PacketFactory.java
+++ src/main/java/com/dsh105/holoapi/util/PacketFactory.java
@@ -4,7 +4,7 @@ import com.dsh105.holoapi.util.wrapper.protocol.*;
 
 public class PacketFactory {
 
-    public static final Packet ENTITY_SPAWN = new Packet(Protocol.PLAY, Sender.SERVER, 14
-    public static final Pcket ENTITY_ATTACH Packet(Protocol.PLAY, Sender.SERVER, 27);
+    public static final Packet ENTITY_SPAWN = new Packet(Protocol.PLAY, Sender.SERVER, 14);
+    public static final Packet ENTITY_ATTACH = new Packet(Protocol.PLAY, Sender.SERVER, 27);
 
 }
