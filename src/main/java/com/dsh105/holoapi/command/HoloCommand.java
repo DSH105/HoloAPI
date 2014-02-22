46f3dad7bb5bb7146f4c69115ae1088988c6a517
DSH105 <jason.chirgwin@icloud.com>
2014-02-22 22:00:50 +1100

    Command stuff for testing

--- src/main/java/com/dsh105/holoapi/command/HoloCommand.java
+++ src/main/java/com/dsh105/holoapi/command/HoloCommand.java
@@ -2,10 +2,17 @@ package com.dsh105.holoapi.command;
 
 import com.dsh105.dshutils.pagination.Paginator;
 import com.dsh105.dshutils.util.StringUtil;
+import com.dsh105.holoapi.HoloPlugin;
+import com.dsh105.holoapi.api.Hologram;
+import com.dsh105.holoapi.api.HologramFactory;
+import com.dsh105.holoapi.image.ImageChar;
 import com.dsh105.holoapi.util.Lang;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 
@@ -26,7 +33,21 @@ public class HoloCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Lang.sendTo(sender, Lang.COMMAND_ERROR.toString()
+        if (args.length == 0 && sender instanceof Player) {
+            final Player p = (Player) sender;
+            final Hologram h = new HologramFactory().withCoords(p.getLocation().getX(), p.getLocation().getX(), p.getLocation().getX()).withText("Hi, how's it going?", "Morning", ChatColor.BLUE + "" + ImageChar.MEDIUM_SHADE + ImageChar.BLOCK + ChatColor.MAGIC + ImageChar.MEDIUM_SHADE).build();
+            h.show(p);
+            p.sendMessage("Spawned");
+
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    h.remove(p);
+                    p.sendMessage("Despawned");
+                }
+            }.runTaskLater(HoloPlugin.getInstance(), 60L);
+        }
+        Lang.sendTo(sender, Lang.COMMAND_ERROR.getValue()
                 .replace("%cmd%", "/" + cmd.getLabel() + " " + (args.length == 0 ? "" : StringUtil.combineSplit(0, args, " "))));
         return true;
     }
