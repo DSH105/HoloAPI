7e640c72808398b68cbaaeb717bd117411e8217e
CaptainBern <CaptainBern@users.noreply.github.com>
2014-02-22 22:53:33 +0100

    Create PacketFactory.java

---
+++ src/main/java/com/dsh105/holoapi/util/PacketFactory.java
@@ -0,0 +1,10 @@
+package com.dsh105.holoapi.util;
+
+import com.dsh105.holoapi.util.wrapper.protocol.*;
+
+public class PacketFactory {
+
+    public static final Packet ENTITY_SPAWN = new Packet(Protocol.PLAY, Sender.SERVER, 14
+    public static final Pcket ENTITY_ATTACH Packet(Protocol.PLAY, Sender.SERVER, 27);
+
+}
