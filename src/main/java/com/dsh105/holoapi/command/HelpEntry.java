f01abe9396a34fe98654076e5064327743db938b
DSH105 <jason.chirgwin@icloud.com>
2014-02-28 21:45:13 +1100

    Add new commands to the help menu

--- src/main/java/com/dsh105/holoapi/command/HelpEntry.java
+++ src/main/java/com/dsh105/holoapi/command/HelpEntry.java
@@ -8,6 +8,9 @@ public enum HelpEntry {
     CREATE_IMAGE("/holo create image <image_id>", "Create a holographic display with the specified images. Images can be defined in the config.yml."),
     REMOVE("/holo remove <id>", "Remove an existing holographic display using its ID."),
     INFO("/holo info", "View information on active holographic displays."),
+    MOVE("/holo move <id>", "Move a hologram to your current position."),
+    TELEPORT("/holo teleport <id>", "Teleport to a specific hologram."),
+    BUILD("/holo build", "Dynamically build a combined hologram of both text and images."),
     RELOAD("/holo reload", "Reload all HoloAPI configuration files."),
     ;
 
