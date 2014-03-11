cb18247f93abd82e738bdae603e7e51252fa7685
CaptainBern <captainberneby@gmail.com>
2014-03-11 20:37:57 +0100

    Added utility to read contents of a website

---
+++ src/main/java/com/dsh105/holoapi/util/WebUtil.java
@@ -0,0 +1,25 @@
+package com.dsh105.holoapi.util;
+
+import net.minecraft.util.org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class WebUtil {
+
+    public static String readWebsiteContentsSoWeCanUseTheText(String link) {
+        try {
+            URL url = new URL(link);
+            URLConnection con = url.openConnection();
+            InputStream in = con.getInputStream();
+            String encoding = con.getContentEncoding();
+            encoding = encoding == null ? "UTF-8" : encoding;
+            String body = IOUtils.toString(in, encoding);
+            return body;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
