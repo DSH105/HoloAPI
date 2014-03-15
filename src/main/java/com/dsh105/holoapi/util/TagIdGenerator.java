6a1abe09dc42cd910cb1bd1e5e62cc8b83d0eb2f
DSH105 <jason.chirgwin@icloud.com>
2014-03-15 18:37:18 +1100

    Fix holograms disappearing and doing weird stuff. Fixes #14
    No more holograms with duplicate IDs ;D

--- src/main/java/com/dsh105/holoapi/util/TagIdGenerator.java
+++ src/main/java/com/dsh105/holoapi/util/TagIdGenerator.java
@@ -1,21 +1,35 @@
 package com.dsh105.holoapi.util;
 
+import java.util.ArrayList;
+
 public class TagIdGenerator {
 
-    private static int nextId = Short.MAX_VALUE;
-    private static int simpleId = Short.MIN_VALUE;
+    private static ArrayList<Integer> REGISTERED = new ArrayList<Integer>();
+    private volatile static int SHARED_ID = Short.MAX_VALUE;
+    private volatile static int SHARED_SIMPLE_ID = Short.MIN_VALUE;
 
     public static int nextId(int counter) {
-        nextId += counter * 2;
-        return nextId++;
+        return nextId(counter, false);
     }
 
     public static int nextSimpleId(int counter) {
-        simpleId += counter * 2;
-        if (simpleId >= 0) {
-            simpleId = Short.MIN_VALUE;
-            return nextSimpleId(counter);
+        return nextId(counter, true);
+    }
+
+    private static int nextId(int counter, boolean simple) {
+        int firstId = simple ? ++SHARED_SIMPLE_ID : ++SHARED_ID;
+        for (int i = 0; i <= (counter * 2); i++) {
+            if (simple && (firstId + i) > 0) {
+                SHARED_SIMPLE_ID = Short.MIN_VALUE;
+                return nextId(counter, simple);
+            }
+
+            if (REGISTERED.contains(firstId + i)) {
+                return nextId(counter);
+            } else {
+                REGISTERED.add(firstId + i);
+            }
         }
-        return simpleId++;
+        return firstId;
     }
 }
