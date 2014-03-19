5c1bca96c24c94c1fe627fdf4b029b11dfd5a2b8
CaptainBern <captainberneby@gmail.com>
2014-03-19 11:58:21 +0100

    Fix for 1.6

--- src/main/java/com/dsh105/holoapi/util/wrapper/WrappedDataWatcher.java
+++ src/main/java/com/dsh105/holoapi/util/wrapper/WrappedDataWatcher.java
@@ -1,5 +1,6 @@
 package com.dsh105.holoapi.util.wrapper;
 
+import com.dsh105.holoapi.HoloAPI;
 import com.dsh105.holoapi.util.ReflectionUtil;
 
 import java.lang.reflect.InvocationTargetException;
@@ -8,7 +9,11 @@ public class WrappedDataWatcher extends AbstractWrapper {
 
     public WrappedDataWatcher() {
         try {
-            super.setHandle(ReflectionUtil.getNMSClass("DataWatcher").getConstructor(ReflectionUtil.getNMSClass("Entity")).newInstance(new Object[]{null}));
+            if(HoloAPI.isUsingNetty) {
+                super.setHandle(ReflectionUtil.getNMSClass("DataWatcher").getConstructor(ReflectionUtil.getNMSClass("Entity")).newInstance(new Object[]{null}));
+            } else {
+                super.setHandle(ReflectionUtil.getNMSClass("DataWatcher").newInstance());
+            }
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
