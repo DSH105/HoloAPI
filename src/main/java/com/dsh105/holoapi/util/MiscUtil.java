af218220ec4897c82749d1b70ae558971aa82763
CaptainBern <captainberneby@gmail.com>
2014-03-19 11:52:12 +0100

    Should fix it... Hopefully o/

--- src/main/java/com/dsh105/holoapi/util/MiscUtil.java
+++ src/main/java/com/dsh105/holoapi/util/MiscUtil.java
@@ -1,12 +1,14 @@
 package com.dsh105.holoapi.util;
 
+import com.google.common.collect.BiMap;
 import net.minecraft.util.org.apache.commons.io.IOUtils;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
-public class WebUtil {
+public class MiscUtil {
 
     public static String readWebsiteContentsSoWeCanUseTheText(String link) {
         try {
@@ -18,6 +20,18 @@ public class WebUtil {
             return IOUtils.toString(in, encoding);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static <K, V> K getKeyAtValue(Map<K, V> map, V value) {
+        if(map instanceof BiMap) {
+            return ((BiMap<K, V>) map).inverse().get(value);
+        }
+        for(Map.Entry<K, V> entry : map.entrySet()) {
+            if(entry.getValue().equals(value)) {
+                return entry.getKey();
+            }
         }
         return null;
     }
