64993378920fd7ef0e45706d0da158754c019869
CaptainBern <captainberneby@gmail.com>
2014-07-02 17:16:42 +0200

    Updated license notes

--- src/main/java/com/captainbern/hug/Constant.java
+++ src/main/java/com/captainbern/hug/Constant.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Hug.
+ *
+ * Hug is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hug is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hug (or the project it is being used in).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.captainbern.hug;
 
 public class Constant {
